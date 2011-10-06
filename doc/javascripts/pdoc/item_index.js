0e5092b7133d754901ebabb799ed0c48a379ce7f
Aleksey V Zapparov <ixti@member.fsf.org>
2011-10-06 21:22:54 +0200

    Bundled documentation

---
+++ doc/javascripts/pdoc/item_index.js
@@ -0,0 +1,8 @@
+if (!window.PDoc) window.PDoc = {};
+PDoc.elements = {
+  'FsTools': { 'name': 'FsTools', 'type': 'namespace', 'path': 'FsTools/index.html' },
+'FsTools.walk': { 'name': 'FsTools.walk', 'type': 'class method', 'path': 'FsTools/walk/index.html' },
+'FsTools.rm_rf': { 'name': 'FsTools.rm_rf', 'type': 'class method', 'path': 'FsTools/rm_rf/index.html' },
+'FsTools.mkdir_p': { 'name': 'FsTools.mkdir_p', 'type': 'class method', 'path': 'FsTools/mkdir_p/index.html' },
+'FsTools.copy': { 'name': 'FsTools.copy', 'type': 'class method', 'path': 'FsTools/copy/index.html' }
+};
