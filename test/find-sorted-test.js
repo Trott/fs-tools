a63d77bf4d4e6b231f420ee3a152c164d323f250
Rich Trott <rtrott@gmail.com>
2019-06-16 06:33:56 -0700

    chore: Replace vows with tape

--- test/find-sorted-test.js
+++ test/find-sorted-test.js
@@ -3,31 +3,27 @@
 
 var FsTools = require('../');
 var Helper  = require('./helper');
-var Assert  = require('assert');
+
+var test = require('tape');
 
 var SANDBOX = Helper.SANDBOX_DIR + '/find-sorted';
 
-require('vows').describe('findSorted()').addBatch({
-  'found files in deirectory': {
-    topic: function () {
-      FsTools.findSorted(SANDBOX, this.callback);
-    },
+test('found files in directory', function (t) {
+  t.plan(2);
+  FsTools.findSorted(SANDBOX, function (err, files) {
+    t.ok(!err, 'Has no errors');
 
-    'should return sorted list of file': function (err, files) {
-      Assert.ok(!err, 'Has no errors');
+    files = files.map(function (f) { return f.replace(SANDBOX, ''); });
 
-      files = files.map(function (f) { return f.replace(SANDBOX, ''); });
-
-      Assert.deepEqual(files, [
-        '/file',
-        '/foo/bar/baz/file',
-        '/foo/bar/baz/link',
-        '/foo/bar/file',
-        '/foo/bar/link',
-        '/foo/file',
-        '/foo/link',
-        '/link'
-      ]);
-    }
-  }
-}).export(module);
+    t.deepEqual(files, [
+      '/file',
+      '/foo/bar/baz/file',
+      '/foo/bar/baz/link',
+      '/foo/bar/file',
+      '/foo/bar/link',
+      '/foo/file',
+      '/foo/link',
+      '/link'
+    ]);
+  });
+});
